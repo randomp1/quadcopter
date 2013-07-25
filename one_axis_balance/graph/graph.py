13129c7e1f409ef47606c393a7e85831f52e6e8f
Alex Seaton <randomp1@hotmail.co.uk>
2013-07-25 23:24:42 +0100

    Added block to list serial ports and allow user to choose one

--- one_axis_balance/graph/graph.py
+++ one_axis_balance/graph/graph.py
@@ -1,19 +1,40 @@
 import serial
+from serial.tools import list_ports
 import time
-import numpy
 import struct
+
+###############################################################################
+#                                                                             #
+#                           Function definitions                              #
+#                                                                             #
+###############################################################################
 
 def alignment(port):
 	x = 'h'
 	while x != '\n':
 		x = port.read()
 
-#TODO: detect port actually used; it seems my default is 3
+###############################################################################
+#                                                                             #
+#                           Start of main code                                #
+#                                                                             #
+###############################################################################
+
+#Find list of available ports
+print("Ports which appear to have a USB device attached:\n")
+
+portList = list(list_ports.grep('USB'))
+for i in enumerate(portList): print("Device " + str(i[0]+1) + ": " + "\nHardware info: " + i[1][2] + "\n")
+
+#Ask user which one they want to use
+print("Enter the number of the device you wish to connect to: ")
+portNum = int(raw_input('>'))
+
 #Open serial port
-newPort = serial.Serial('/dev/ttyACM3',115200)
+newPort = serial.Serial(portList[portNum-1][0],115200)
 print("Opened " + newPort.portstr)
 
-#Check if arduino has sent initialisation character
+#TODO: Check if arduino has sent initialisation character
 time.sleep(1)
 #TODO: exit program if arduino does not send initialisation character after a reasonable wait
 #Send character to tell arduino to start sending data
@@ -25,35 +46,40 @@ newPort.flushInput()
 b = [0]*47
 #Align with starting position of data packet
 alignment(newPort)
-for i in range(10):
-	newPort.readinto(b)
-	if ((b[0] == '$') and (b[45] == '\r') and (b[46] == '\n')):
-		print "Packets aligned properly! :)"
-		str_b = ''.join(str(e) for e in b[1:5])
-		print struct.unpack('>I',str_b)
-		str_b = ''.join(str(e) for e in b[5:9])
-		print struct.unpack('>f',str_b)
-		str_b = ''.join(str(e) for e in b[9:13])
-		print struct.unpack('>f',str_b)
-		str_b = ''.join(str(e) for e in b[13:17])
-		print struct.unpack('>f',str_b)
-		str_b = ''.join(str(e) for e in b[17:21])
-		print struct.unpack('>f',str_b)
-		str_b = ''.join(str(e) for e in b[21:25])
-		print struct.unpack('>f',str_b)
-		str_b = ''.join(str(e) for e in b[25:29])
-		print struct.unpack('>f',str_b)
-		str_b = ''.join(str(e) for e in b[29:33])
-		print struct.unpack('>f',str_b)
-		str_b = ''.join(str(e) for e in b[33:37])
-		print struct.unpack('>f',str_b)
-		str_b = ''.join(str(e) for e in b[37:41])
-		print struct.unpack('>f',str_b)
-		str_b = ''.join(str(e) for e in b[41:45])
-		print struct.unpack('>f',str_b)
-	else:
-		print "Packets not properly aligned :("
-		alignment(newPort)
-		continue
-	#convert appropriate sections of b to a stirng
+x = newPort.readline()
+print(x)
+
+#for i in range(10):
+#	newPort.readinto(b)
+#	print b
+#	if ((b[0] == '$') and (b[45] == '\r') and (b[46] == '\n')):
+#		print "Packets aligned properly! :)"
+#		
+#	else:
+#		print "Packets not properly aligned :("
+#		alignment(newPort)
+#		continue
+#	#convert appropriate sections of b to a stirng
+#	str_b = ''.join(str(e) for e in b[1:5])
+#	print struct.unpack('>I',str_b)
+#	str_b = ''.join(str(e) for e in b[5:9])
+#	print struct.unpack('>f',str_b)
+#	str_b = ''.join(str(e) for e in b[9:13])
+#	print struct.unpack('>f',str_b)
+#	str_b = ''.join(str(e) for e in b[13:17])
+#	print struct.unpack('>f',str_b)
+#	str_b = ''.join(str(e) for e in b[17:21])
+#	print struct.unpack('>f',str_b)
+#	str_b = ''.join(str(e) for e in b[21:25])
+#	print struct.unpack('>f',str_b)
+#	str_b = ''.join(str(e) for e in b[25:29])
+#	print struct.unpack('>f',str_b)
+#	str_b = ''.join(str(e) for e in b[29:33])
+#	print struct.unpack('>f',str_b)
+#	str_b = ''.join(str(e) for e in b[33:37])
+#	print struct.unpack('>f',str_b)
+#	str_b = ''.join(str(e) for e in b[37:41])
+#	print struct.unpack('>f',str_b)
+#	str_b = ''.join(str(e) for e in b[41:45])
+#	print struct.unpack('>f',str_b)
 newPort.close()
